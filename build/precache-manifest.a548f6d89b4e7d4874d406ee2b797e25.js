08a9ffe7222e7604e48f826cb87f8f801270f8bf
mcatzin <catzin21@gmail.com>
2020-02-09 10:49:23 -0800

    adding robotfriends project

---
+++ build/precache-manifest.a548f6d89b4e7d4874d406ee2b797e25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b21621b5dce7cf161b35f1eb52d1c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1e9a99f40c6a2cc7f90a",
+    "url": "/static/css/main.3d6ae6f4.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9a99f40c6a2cc7f90a",
+    "url": "/static/js/main.4d13334a.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
